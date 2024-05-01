4b18ad9ab073c0beba7b2c80909205d7f2ed7afe
Daniel Popovici <pop.4.daniel@gmail.com>
2024-05-01 23:49:08 +0200

    Add requests to component

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,13 @@
 import pluginJs from "@eslint/js";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import globals from "globals";
 
 
 export default [
   {
     ignores : ["*out/*"]
   },
-  {
-    
-  },
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   pluginReactConfig,
   {
