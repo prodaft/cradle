449dbe38a0f5c94098f497ed64c00e6d9661bdb6
Babu-on-Github <d.i.popovici-1@student.tudelft.nl>
2024-05-01 23:49:08 +0200

    Add requests to component

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,15 +1,37 @@
-import { useState } from 'react'
-
+import { useState , useEffect } from 'react'
+import axios from 'axios';
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(null)
+
+  const fetchNumber = async () => {
+    try {
+      const response = await axios.get('http://localhost:3010/counter');
+      setCount(response.data.count);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const updateNumber = async () => {
+    try {
+      const response = await axios.post('http://localhost:3010/increment',  count + 1);
+      setCount(response.data.count);
+    } catch (error) {
+      console.error('Error creating data:', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchNumber();
+  }, []);
 
   return (
     <>
       <h1>Counter</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={updateNumber}>
           count is {count}
         </button>
       </div>
