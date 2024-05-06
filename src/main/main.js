efddfab388c62fac2ea08da976443be41a494b7c
Babu-on-Github <d.i.popovici-1@student.tudelft.nl>
2024-05-06 18:51:28 +0200

    Template project

---
+++ src/main/main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const rendererDirectory = path.join(__dirname, '..', 'renderer');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({});
+  mainWindow.loadURL('http://localhost:5173');
+  // mainWindow.loadFile(path.join(rendererDirectory, 'index.html'));
+  mainWindow.webContents.openDevTools();
+  mainWindow.on('closed', () => mainWindow = null);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow == null) {
+    createWindow();
+  }
+});
