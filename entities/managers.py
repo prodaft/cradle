d2a0d82c7a12feecd3e453f4f9af921f88f61de5
klinashka <diacicov.calin@gmail.com>
2024-05-20 23:22:04 +0200

    Query Cases and Entries Endpoint

--- entities/managers.py
+++ entities/managers.py
@@ -4,6 +4,42 @@ from django.db.models import FilteredRelation, Q, F
 from .enums import EntityType
 
 
+class EntityManager(models.Manager):
+    def get_filtered_entities(
+        self,
+        query_set: models.QuerySet,
+        entity_types: list[str],
+        entity_subtypes: list[str],
+        name_prefix: str,
+    ) -> models.QuerySet:
+        """For a given initial query_set, a list of entity types, a list of entity
+        subtypes and a name prefix, filter the initial query set to keep only
+        entities which have the entity type in entity_types, the entries which
+        have the the subtype in entity_subtypes and the name starting with
+        name_prefix.
+
+        Args:
+            query_set: the query_set on which the additional filters are applied.
+            entityTypes: the entity types on which the QuerySet is filtered.
+            entitySubtypes: the entity subtypes on which the QuerySet is filtered.
+            name_prefix: the name_prefix on which the QuerySet is filtered.
+
+        Returns:
+            a QuerySet instance, filtered according to the entity types, entity
+            subtypes and name prefix specified
+        """
+        return (
+            # filter entities by entity type
+            query_set.filter(type__in=entity_types)
+            # exclude entries of wrong type.
+            .exclude(
+                Q(type=EntityType.ENTRY) & ~Q(subtype__in=entity_subtypes),
+            )
+            # filter name
+            .filter(name__startswith=name_prefix)
+        )
+
+
 class CaseManager(models.Manager):
     def get_queryset(self) -> models.QuerySet:
         return super().get_queryset().filter(type=EntityType.CASE)
---
+++ entities/tests/test_query_list_view.py
@@ -0,0 +1,147 @@
+from django.test import TestCase
+from user.models import CradleUser
+from django.urls import reverse
+from rest_framework.parsers import JSONParser
+from rest_framework.test import APIClient
+import io
+from rest_framework_simplejwt.tokens import AccessToken
+
+from ..models import Entity
+from ..enums import EntityType, EntitySubtype
+from user.models import Access
+from user.enums import AccessType
+
+
+def bytes_to_json(data):
+    return JSONParser().parse(io.BytesIO(data))
+
+
+class QueryListTest(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.admin_user = CradleUser.objects.create_superuser(
+            username="admin", password="password"
+        )
+        self.normal_user = CradleUser.objects.create_user(
+            username="user", password="password"
+        )
+        self.token_admin = str(AccessToken.for_user(self.admin_user))
+        self.token_normal = str(AccessToken.for_user(self.normal_user))
+        self.headers_admin = {"HTTP_AUTHORIZATION": f"Bearer {self.token_admin}"}
+        self.headers_normal = {"HTTP_AUTHORIZATION": f"Bearer {self.token_normal}"}
+        self.__init_database()
+
+    def __init_database(self):
+        self.cases = [
+            Entity.objects.create(
+                name=f"Case {i}", description=f"{i}", type=EntityType.CASE
+            )
+            for i in range(0, 4)
+        ]
+        self.entries = []
+        self.entries.append(
+            Entity.objects.create(
+                name="Entry1",
+                description="1",
+                type=EntityType.ENTRY,
+                subtype=EntitySubtype.IPV4,
+            )
+        )
+        self.entries.append(
+            Entity.objects.create(
+                name="EnTry2",
+                description="2",
+                type=EntityType.ENTRY,
+                subtype=EntitySubtype.PASSWORD,
+            )
+        )
+
+        Access.objects.create(
+            user=self.normal_user, case=self.cases[0], access_type=AccessType.READ_WRITE
+        )
+        Access.objects.create(
+            user=self.normal_user, case=self.cases[1], access_type=AccessType.READ
+        )
+        Access.objects.create(
+            user=self.normal_user, case=self.cases[2], access_type=AccessType.NONE
+        )
+
+    def test_query_not_authenticated(self):
+        response = self.client.get(reverse("query_list"))
+
+        self.assertEqual(response.status_code, 401)
+
+    def test_query_shows_only_entities_with_access_admin(self):
+        response = self.client.get(reverse("query_list"), **self.headers_admin)
+
+        result = list(bytes_to_json(response.content))
+        self.assertEqual(len(result), 6)
+
+    def test_query_shows_only_entities_with_access_normal(self):
+
+        response = self.client.get(reverse("query_list"), **self.headers_normal)
+
+        result = list(bytes_to_json(response.content))
+        self.assertEqual(len(result), 4)
+
+    def test_query_filters_entites_based_on_type(self):
+
+        query_params = {"entityType": ["entry"]}
+
+        response = self.client.get(
+            reverse("query_list"), query_params, **self.headers_normal
+        )
+        result = list(bytes_to_json(response.content))
+        with self.subTest("Correct amount"):
+            self.assertEqual(len(result), 2)
+
+        for entity in result:
+            with self.subTest("Correct type"):
+                self.assertEqual(entity["type"], "entry")
+        with self.subTest("Status code"):
+            self.assertEqual(response.status_code, 200)
+
+    def test_query_filters_entities_based_on_subtype(self):
+        query_params = {"entityType": ["entry"], "entitySubtype": ["ip_v4"]}
+        response = self.client.get(
+            reverse("query_list"), query_params, **self.headers_normal
+        )
+        result = list(bytes_to_json(response.content))
+
+        with self.subTest("Correct number of results"):
+            self.assertEqual(len(result), 1)
+
+        with self.subTest("Correct subtype"):
+            for entity in result:
+                self.assertEqual(entity["subtype"], "ip_v4")
+
+        with self.subTest("Status code"):
+            self.assertEqual(response.status_code, 200)
+
+    def test_query_filters_entities_based_on_name(self):
+        query_params = {"entityType": ["entry"], "name": "Entr"}
+
+        response = self.client.get(
+            reverse("query_list"), query_params, **self.headers_normal
+        )
+        result = list(bytes_to_json(response.content))
+
+        with self.subTest("Correct number of results"):
+            self.assertEqual(len(result), 1)
+
+        with self.subTest("Correct name"):
+            for entity in result:
+                self.assertTrue(entity["name"].startswith("Entr"))
+
+        with self.subTest("Status code"):
+            self.assertEqual(response.status_code, 200)
+
+    def test_query_filters_invalid_params(self):
+        query_params = {"entityType": ["entr"]}
+        response = self.client.get(
+            reverse("query_list"), query_params, **self.headers_normal
+        )
+
+        with self.subTest("Status code"):
+            self.assertEqual(response.status_code, 400)
