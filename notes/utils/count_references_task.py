2487ae9a2c9788b881c9f6821918c51c63f823c3
TudorMagirescu <tudor.magirescu@gmail.com>
2024-05-20 12:28:02 +0200

    Count References in Notes

---
+++ notes/utils/count_references_task.py
@@ -0,0 +1,36 @@
+from typing import Dict, Set
+from entities.models import Entity
+
+
+class CountReferencesTask:
+
+    def run(
+        self, referenced_entities: Dict[str, Set[Entity]]
+    ) -> Dict[str, Set[Entity]]:
+        """Checks that the note references at least one case and at least 2 entities
+        (including the referenced cases).
+
+        Args:
+            referenced_entities: Dictionary containing sets of the entities being
+            referenced in the note sent by the user.
+
+        Returns:
+            An updated dictionary containing sets of the entities. In the case of this
+            task, the dictionary is not updated.
+
+        Raises:
+            ValueError: The note does not reference at least one case and at least
+            two entities.
+        """
+
+        referenced_cases = len(referenced_entities["case"])
+        reference_count = sum(
+            len(entity_set) for entity_set in referenced_entities.values()
+        )
+
+        if referenced_cases == 0 or reference_count < 2:
+            raise ValueError(
+                "Note does not reference at least one case and at least two entities."
+            )
+
+        return referenced_entities
