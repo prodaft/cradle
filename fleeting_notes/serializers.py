6e23b2852506107d75e9770e494ddf9b5ce99410
Babu-on-Github <d.i.popovici-1@student.tudelft.nl>
2024-05-31 22:22:28 +0200

    Custom serializer for truncated content

--- fleeting_notes/serializers.py
+++ fleeting_notes/serializers.py
@@ -8,6 +8,31 @@ class FleetingNoteRetrieveSerializer(serializers.ModelSerializer):
         fields = ["id", "content", "last_edited"]
 
 
+class FleetingNoteTruncatedRetrieveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FleetingNote
+        fields = ["id", "content", "last_edited"]
+
+    def to_representation(self, instance):
+        """
+        Overrides the default to_representation method to truncate the content
+        field of the FleetingNote entity to 200 characters.
+
+        Args:
+            instance: the FleetingNote entity to be serialized
+
+        Returns:
+            A dictionary containing the serialized FleetingNote entity
+        """
+        representation = super().to_representation(instance)
+        representation["content"] = (
+            representation["content"][:200] + "..."
+            if len(representation["content"]) > 200
+            else representation["content"]
+        )
+        return representation
+
+
 class FleetingNoteCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = FleetingNote
