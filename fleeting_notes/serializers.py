adaee41c2ecdada1baea38a871b2c4f6f3b9f16b
Daniel Popovici <pop.4.daniel@gmail.com>
2024-05-31 18:57:33 +0200

    Add tests and docs

--- fleeting_notes/serializers.py
+++ fleeting_notes/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class FleetingNoteRetrieveSerializer(serializers.ModelSerializer):
     class Meta:
         model = FleetingNote
-        fields = ["id", "content", "last_updated"]
+        fields = ["id", "content", "last_edited"]
 
 
 class FleetingNoteCreateSerializer(serializers.ModelSerializer):
@@ -14,6 +14,18 @@ class FleetingNoteCreateSerializer(serializers.ModelSerializer):
         fields = ["content"]
 
     def create(self, validated_data):
+        """
+        Creates a new FleetingNote entity based on the validated data.
+        Moreover, it sets the user field to correspond to the
+        authenticated user.
+
+        Args:
+            validated_data: a dictionary containing the attributes of
+                the FleetingNote entity
+
+        Returns:
+            The created FleetingNote entity
+        """
         user = self.context["request"].user
         validated_data["user"] = user
         return super().create(validated_data)
