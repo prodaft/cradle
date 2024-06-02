85ec4b93a40eddce56a2775a5b0962f60a155fd3
Daniel Popovici <pop.4.daniel@gmail.com>
2024-06-02 15:42:48 +0200

    Resolve problems

--- fleeting_notes/models.py
+++ fleeting_notes/models.py
@@ -6,4 +6,4 @@ from user.models import CradleUser
 class FleetingNote(models.Model):
     content: models.CharField = models.CharField()
     last_edited: models.DateTimeField = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(CradleUser, on_delete=models.CASCADE, to_field="id")
+    user: models.ForeignKey = models.ForeignKey(CradleUser, on_delete=models.CASCADE)
