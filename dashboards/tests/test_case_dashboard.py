18cf3da60c844f467ff4f0885f8bb509149d258d
Matei Grigore <m.grigore@student.tudelft.nl>
2024-06-12 11:42:58 +0200

    Case dashboard

--- dashboards/tests/test_case_dashboard.py
+++ dashboards/tests/test_case_dashboard.py
@@ -1,14 +1,11 @@
-from user.models import CradleUser
 from django.urls import reverse
 from rest_framework.parsers import JSONParser
 from rest_framework.test import APIClient
 import io
-from rest_framework_simplejwt.tokens import AccessToken
-from .utils import DashboardsTestCase
 
+from .utils import DashboardsTestCase
 from entities.models import Entity
 from entities.enums import EntityType
-from access.models import Access
 from notes.models import Note
 
 
@@ -18,115 +15,28 @@ def bytes_to_json(data):
 
 class GetCaseDashboardTest(DashboardsTestCase):
 
-    def check_note_ids(self, notes, notes_json):
-        with self.subTest("Check number of notes"):
-            self.assertEqual(len(notes), len(notes_json))
+    def check_ids(self, entities, entities_json):
+        with self.subTest("Check number of entities"):
+            self.assertEqual(len(entities), len(entities_json))
 
-        for i in range(0, len(notes_json)):
-            with self.subTest("Check note id"):
-                note = notes_json[i]
-                self.assertEqual(note["id"], notes[i].id)
+        for i in range(0, len(entities_json)):
+            with self.subTest("Check entity id"):
+                entity = entities_json[i]
+                self.assertEqual(entity["id"], entities[i].id)
 
-    def check_case_ids(self, cases, cases_json):
-        with self.subTest("Check number of cases"):
-            self.assertEqual(len(cases), len(cases_json))
+    def check_inaccessible_cases_name(self, inaccessible_cases):
+        for case in inaccessible_cases:
+            with self.subTest("Check case anonymous names"):
+                self.assertEqual(case["name"], "Some Case")
+                self.assertEqual(case["description"], "Some Description")
 
-        for i in range(0, len(cases_json)):
-            with self.subTest("Check case id"):
-                case = cases_json[i]
-                self.assertEqual(case["id"], cases[i].id)
+    def update_notes(self):
+        self.note3 = Note.objects.create(content="Note3")
+        self.note3.entities.add(self.case3, self.actor2, self.metadata1)
 
-    def check_actor_ids(self, actors, actors_json):
-        with self.subTest("Check number of actors"):
-            self.assertEqual(len(actors), len(actors_json))
-
-        for i in range(0, len(actors_json)):
-            with self.subTest("Check actor id"):
-                actor = actors_json[i]
-                self.assertEqual(actor["id"], actors[i].id)
-
-    def check_metadata_ids(self, metadata, metadata_json):
-        with self.subTest("Check number of metadata"):
-            self.assertEqual(len(metadata), len(metadata_json))
-
-        for i in range(0, len(metadata_json)):
-            with self.subTest("Check metadata id"):
-                metadata_response = metadata_json[i]
-                self.assertEqual(metadata_response["id"], metadata[i].id)
-
-    def check_entry_ids(self, entries, entries_json):
-        with self.subTest("Check number of entries"):
-            self.assertEqual(len(entries), len(entries_json))
-
-        for i in range(0, len(entries_json)):
-            with self.subTest("Check entry id"):
-                entry = entries_json[i]
-                self.assertEqual(entry["id"], entries[i].id)
-
-    def create_users(self):
-        self.admin_user = CradleUser.objects.create_superuser(
-            username="admin", password="password", is_staff=True
-        )
-        self.user1 = CradleUser.objects.create_user(
-            username="user1", password="password", is_staff=False
-        )
-        self.user2 = CradleUser.objects.create_user(
-            username="user2", password="password", is_staff=False
-        )
-
-    def create_tokens(self):
-        self.token_user2 = str(AccessToken.for_user(self.user2))
-        self.token_admin = str(AccessToken.for_user(self.admin_user))
-        self.token_user1 = str(AccessToken.for_user(self.user1))
-        self.headers_admin = {"HTTP_AUTHORIZATION": f"Bearer {self.token_admin}"}
-        self.headers_user1 = {"HTTP_AUTHORIZATION": f"Bearer {self.token_user1}"}
-        self.headers_user2 = {"HTTP_AUTHORIZATION": f"Bearer {self.token_user2}"}
-
-    def create_notes(self):
-        self.note1 = Note.objects.create(content="Note1")
-        self.note1.entities.add(self.case1, self.actor1, self.metadata1)
-
-        self.note2 = Note.objects.create(content="Note2")
-        self.note2.entities.add(self.case2, self.actor2, self.case1)
-
-    def create_cases(self):
-        self.case1 = Entity.objects.create(
-            name="Case1", description="Description1", type=EntityType.CASE
-        )
-
-        self.case2 = Entity.objects.create(
-            name="Case2", description="Description2", type=EntityType.CASE
-        )
-
-    def create_actors(self):
-        self.actor1 = Entity.objects.create(
-            name="Actor1", description="Description1", type=EntityType.ACTOR
-        )
-
-        self.actor2 = Entity.objects.create(
-            name="Actor2", description="Description2", type=EntityType.ACTOR
-        )
-
-    def create_metadata(self):
-        self.metadata1 = Entity.objects.create(
-            name="Metadata1", description="Description1", type=EntityType.METADATA
-        )
-
-    def create_access(self):
-        self.access1 = Access.objects.create(
-            user=self.user1, case=self.case1, access_type="read-write"
-        )
-
-        self.access2 = Access.objects.create(
-            user=self.user1, case=self.case2, access_type="read"
-        )
-
-        self.access3 = Access.objects.create(
-            user=self.user2, case=self.case1, access_type="read"
-        )
-
-        self.access4 = Access.objects.create(
-            user=self.user2, case=self.case2, access_type="none"
+    def update_cases(self):
+        self.case3 = Entity.objects.create(
+            name="Case3", description="Description3", type=EntityType.CASE
         )
 
     def setUp(self):
@@ -134,19 +44,8 @@ class GetCaseDashboardTest(DashboardsTestCase):
 
         self.client = APIClient()
 
-        self.create_users()
-
-        self.create_tokens()
-
-        self.create_cases()
-
-        self.create_actors()
-
-        self.create_metadata()
-
-        self.create_access()
-
-        self.create_notes()
+        self.update_cases()
+        self.update_notes()
 
     def test_get_dashboard_admin(self):
         response = self.client.get(
@@ -155,11 +54,12 @@ class GetCaseDashboardTest(DashboardsTestCase):
         )
         self.assertEqual(response.status_code, 200)
 
-        notes = Note.objects.all().order_by("-timestamp")
-        cases = Entity.objects.filter(type=EntityType.CASE).exclude(id=self.case1.id)
+        notes = Note.objects.exclude(id=self.note3.id).order_by("-timestamp")
+        cases = Entity.objects.filter(type=EntityType.CASE).filter(id=self.case2.id)
         actors = Entity.objects.filter(type=EntityType.ACTOR)
         metadata = Entity.objects.filter(type=EntityType.METADATA)
         entries = Entity.objects.filter(type=EntityType.ENTRY)
+        inaccessible_cases = Entity.objects.none()
 
         json_response = bytes_to_json(response.content)
 
@@ -169,15 +69,14 @@ class GetCaseDashboardTest(DashboardsTestCase):
         with self.subTest("Check case access"):
             self.assertEqual(json_response["access"], "read-write")
 
-        self.check_note_ids(notes, json_response["notes"])
+        self.check_ids(notes, json_response["notes"])
+        self.check_ids(cases, json_response["cases"])
+        self.check_ids(actors, json_response["actors"])
+        self.check_ids(metadata, json_response["metadata"])
+        self.check_ids(entries, json_response["entries"])
+        self.check_ids(inaccessible_cases, json_response["inaccessible_cases"])
 
-        self.check_case_ids(cases, json_response["cases"])
-
-        self.check_actor_ids(actors, json_response["actors"])
-
-        self.check_metadata_ids(metadata, json_response["metadata"])
-
-        self.check_entry_ids(entries, json_response["entries"])
+        self.check_inaccessible_cases_name(json_response["inaccessible_cases"])
 
     def test_get_dashboard_user_read_access(self):
         response = self.client.get(
@@ -191,6 +90,7 @@ class GetCaseDashboardTest(DashboardsTestCase):
         actors = Entity.objects.filter(id=self.actor1.id)
         metadata = Entity.objects.filter(id=self.metadata1.id)
         entries = Entity.objects.filter(type=EntityType.ENTRY)
+        inaccessible_cases = Entity.objects.filter(id=self.case2.id)
 
         json_response = bytes_to_json(response.content)
 
@@ -200,15 +100,14 @@ class GetCaseDashboardTest(DashboardsTestCase):
         with self.subTest("Check case access"):
             self.assertEqual(json_response["access"], "read")
 
-        self.check_note_ids(notes, json_response["notes"])
+        self.check_ids(notes, json_response["notes"])
+        self.check_ids(cases, json_response["cases"])
+        self.check_ids(actors, json_response["actors"])
+        self.check_ids(metadata, json_response["metadata"])
+        self.check_ids(entries, json_response["entries"])
+        self.check_ids(inaccessible_cases, json_response["inaccessible_cases"])
 
-        self.check_case_ids(cases, json_response["cases"])
-
-        self.check_actor_ids(actors, json_response["actors"])
-
-        self.check_metadata_ids(metadata, json_response["metadata"])
-
-        self.check_entry_ids(entries, json_response["entries"])
+        self.check_inaccessible_cases_name(json_response["inaccessible_cases"])
 
     def test_get_dashboard_user_read_write_access(self):
         response = self.client.get(
@@ -217,11 +116,12 @@ class GetCaseDashboardTest(DashboardsTestCase):
         )
         self.assertEqual(response.status_code, 200)
 
-        notes = Note.objects.all().order_by("-timestamp")
+        notes = Note.objects.exclude(id=self.note3.id).order_by("-timestamp")
         cases = Entity.objects.filter(id=self.case2.id)
         actors = Entity.actors.all()
         metadata = Entity.metadata.all()
         entries = Entity.entries.all()
+        inaccessible_cases = Entity.objects.none()
 
         json_response = bytes_to_json(response.content)
 
@@ -229,16 +129,47 @@ class GetCaseDashboardTest(DashboardsTestCase):
             self.assertEqual(json_response["id"], self.case1.id)
 
         self.assertEqual(json_response["access"], "read-write")
+        self.check_ids(notes, json_response["notes"])
+        self.check_ids(cases, json_response["cases"])
+        self.check_ids(actors, json_response["actors"])
+        self.check_ids(metadata, json_response["metadata"])
+        self.check_ids(entries, json_response["entries"])
+        self.check_ids(inaccessible_cases, json_response["inaccessible_cases"])
 
-        self.check_note_ids(notes, json_response["notes"])
+        self.check_inaccessible_cases_name(json_response["inaccessible_cases"])
 
-        self.check_case_ids(cases, json_response["cases"])
+    def test_get_dashboard_user_multiple_inaccessible(self):
+        self.note2.entities.add(self.case3)
 
-        self.check_actor_ids(actors, json_response["actors"])
+        response = self.client.get(
+            reverse("case_dashboard", kwargs={"case_name": self.case1.name}),
+            **self.headers_user2,
+        )
+        self.assertEqual(response.status_code, 200)
 
-        self.check_metadata_ids(metadata, json_response["metadata"])
+        notes = Note.objects.filter(id=self.note1.id)
+        cases = Entity.objects.none()
+        actors = Entity.objects.filter(id=self.actor1.id)
+        metadata = Entity.objects.filter(id=self.metadata1.id)
+        entries = Entity.objects.filter(type=EntityType.ENTRY)
+        inaccessible_cases = Entity.cases.exclude(id=self.case1.id).order_by("-id")
 
-        self.check_entry_ids(entries, json_response["entries"])
+        json_response = bytes_to_json(response.content)
+
+        with self.subTest("Check case id"):
+            self.assertEqual(json_response["id"], self.case1.id)
+
+        with self.subTest("Check case access"):
+            self.assertEqual(json_response["access"], "read")
+
+        self.check_ids(notes, json_response["notes"])
+        self.check_ids(cases, json_response["cases"])
+        self.check_ids(actors, json_response["actors"])
+        self.check_ids(metadata, json_response["metadata"])
+        self.check_ids(entries, json_response["entries"])
+        self.check_ids(inaccessible_cases, json_response["inaccessible_cases"])
+
+        self.check_inaccessible_cases_name(json_response["inaccessible_cases"])
 
     def test_get_dashboard_user_no_access(self):
         response = self.client.get(
--- dashboards/tests/test_dashboard_utils.py
+++ dashboards/tests/test_dashboard_utils.py
@@ -1,104 +1,40 @@
-from user.models import CradleUser
-
 from entities.models import Entity
-from entities.enums import EntityType, MetadataSubtype
-from access.models import Access
+from entities.enums import EntityType
 from notes.models import Note
 from ..utils.dashboard_utils import DashboardUtils
 from .utils import DashboardsTestCase
 
 
 class DashboardUtilsDashboardJsonTest(DashboardsTestCase):
-
-    def create_users(self):
-        self.admin_user = CradleUser.objects.create_superuser(
-            username="admin", password="password", is_staff=True
-        )
-        self.normal_user = CradleUser.objects.create_user(
-            username="user", password="password", is_staff=False
-        )
-        self.user2 = CradleUser.objects.create_user(
-            username="user2", password="password", is_staff=False
-        )
-
-    def create_notes(self):
-        self.note1 = Note.objects.create(content="Note1")
-        self.note1.entities.set([self.case1, self.actor1, self.metadata1])
-
-        self.note2 = Note.objects.create(content="Note2")
-        self.note2.entities.set([self.case2, self.actor2, self.case1])
-
-    def create_cases(self):
-        self.case1 = Entity.objects.create(
-            name="Case1", description="Description1", type=EntityType.CASE
-        )
-
-        self.case2 = Entity.objects.create(
-            name="Case2", description="Description2", type=EntityType.CASE
-        )
-
-    def create_actors(self):
-        self.actor1 = Entity.objects.create(
-            name="Actor1", description="Description1", type=EntityType.ACTOR
-        )
-
-        self.actor2 = Entity.objects.create(
-            name="Actor2", description="Description2", type=EntityType.ACTOR
-        )
-
-    def create_metadata(self):
-        self.metadata1 = Entity.objects.create(
-            name="Metadata1",
-            description="Description1",
-            type=EntityType.METADATA,
-            subtype=MetadataSubtype.COUNTRY,
-        )
-
-    def create_access(self):
-        self.access1 = Access.objects.create(
-            user=self.normal_user, case=self.case1, access_type="read-write"
-        )
-
-        self.access2 = Access.objects.create(
-            user=self.normal_user, case=self.case2, access_type="none"
-        )
-
-        self.access3 = Access.objects.create(
-            user=self.user2, case=self.case1, access_type="read"
-        )
-
     def setUp(self):
         super().setUp()
-
-        self.create_users()
-
-        self.create_cases()
-
-        self.create_actors()
-
-        self.create_metadata()
-
-        self.create_access()
-
-        self.create_notes()
+        self.case3 = Entity.objects.create(
+            name="Case3", description="Description3", type=EntityType.CASE
+        )
+        self.note3 = Note.objects.create(content="Note3")
+        self.note3.entities.add(self.case3, self.case1)
 
     def test_get_dashboard_json(self):
 
-        notes = Note.objects.filter(id=self.note1.id)
-        actors = Entity.objects.filter(id=self.actor1.id)
-        cases = Entity.objects.none()
+        notes = Note.objects.exclude(id=self.note3.id).order_by("-timestamp")
+        actors = Entity.objects.filter(type=EntityType.ACTOR)
+        cases = Entity.objects.filter(id=self.case2.id)
         metadata = Entity.objects.filter(id=self.metadata1.id)
         entries = Entity.objects.none()
+        inaccessible_cases = Entity.objects.filter(id=self.case3.id)
 
         dashboard_json = DashboardUtils.get_dashboard(
-            user=self.normal_user, entity_id=self.case1.id
+            user=self.user1, entity_id=self.case1.id
         )
 
         self.assertQuerySetEqual(dashboard_json["notes"], notes)
-        self.assertQuerySetEqual(dashboard_json["actors"], actors)
-        self.assertQuerySetEqual(dashboard_json["cases"], cases)
-        self.assertQuerySetEqual(dashboard_json["metadata"], metadata)
-        self.assertQuerySetEqual(dashboard_json["entries"], entries)
+        self.assertQuerySetEqual(dashboard_json["actors"], actors, ordered=False)
+        self.assertQuerySetEqual(dashboard_json["cases"], cases, ordered=False)
+        self.assertQuerySetEqual(dashboard_json["metadata"], metadata, ordered=False)
+        self.assertQuerySetEqual(dashboard_json["entries"], entries, ordered=False)
+        self.assertQuerySetEqual(
+            dashboard_json["inaccessible_cases"], inaccessible_cases, ordered=False
+        )
 
 
 class AddEntityFieldsTest(DashboardsTestCase):
