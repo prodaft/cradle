ad3edafbed392c3bc6f0140f884f50ae84222190
razvand13 <R.D.Dinu-1@student.tudelft.nl>
2024-06-12 16:04:48 +0200

    Destructure all props and add types

--- src/renderer/src/components/NotFound/NotFound.jsx
+++ src/renderer/src/components/NotFound/NotFound.jsx
@@ -2,17 +2,19 @@ import { Link, useLocation } from 'react-router-dom';
 
 /**
  * NotFound component - a placeholder component for pages that are not found.
+ * 
+ * @param {string} message - An optional message to display in place of the default message.
  * @returns {NotFound}
  * @constructor
  */
-export default function NotFound(props) {
+export default function NotFound({ message }) {
     const location = useLocation();
 
     return (
         <div className='flex flex-col justify-center h-full' data-testid='not-found'>
             <h1 className='text-5xl font-bold text-center w-full'>404 Not Found</h1>
-            {props.message ? (
-                <p className='text-center'>{props.message}</p>
+            {message ? (
+                <p className='text-center'>{message}</p>
             ) : (
                 <p className='text-center'>
                     Oops! We can't seem to find a page for{' '}
