9580912deb63f870bbcf5196ad57398757de93a3
Călin-Marian Diacicov <c.m.diacicov@student.tudelft.nl>
2024-06-19 14:02:13 +0200

    Add refresh endpoint

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
-from .views.token_view import TokenObtainPairLogView
+from .views.token_view import TokenObtainPairLogView, TokenRefreshLogView
 
 from django.urls import path
 from .views import user_view
 
 urlpatterns = [
     path("login/", TokenObtainPairLogView.as_view(), name="user_login"),
+    path("refresh/", TokenRefreshLogView.as_view(), name="user_refresh"),
     path("", user_view.UserList.as_view(), name="user_list"),
     path(
         "<int:user_id>/",
