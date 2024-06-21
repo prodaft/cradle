171dbe50581634be8f2717a029d52f95b7c73d88
klinashka <diacicov.calin@gmail.com>
2024-06-21 20:41:42 +0200

    Fixed environment variable issue.

--- jest.config.js
+++ jest.config.js
@@ -138,7 +138,7 @@ const config = {
     // runner: "jest-runner",
 
     // The paths to modules that run some code to configure or set up the testing environment before each test
-    // setupFiles: ['./jest.setup.js'],
+    setupFiles: ['dotenv/config'],
 
     // A list of paths to modules that run some code to configure or set up the testing framework before each test
     // setupFilesAfterEnv: [],
