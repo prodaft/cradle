bf35b07732d6a18ec1168d1a14261af969747e63
razvand13 <R.D.Dinu-1@student.tudelft.nl>
2024-06-21 10:31:00 +0200

    document logo component

--- src/renderer/src/components/Logo/Logo.jsx
+++ src/renderer/src/components/Logo/Logo.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import logo from '../../assets/logo-nobg.png';
 
-export default function Logo({ width = '1.7em', height = '1.7em' }) {
+/**
+ * The Logo component is a simple component that displays the CRADLE logo.
+ *
+ * @param {Object} props
+ * @param {string} [props.width] - the width of the logo
+ * @param {string} [props.height] - the height of the logo
+ * @returns {Logo}
+ * @constructor
+ */
+export default function Logo({ width = 'auto', height = 'auto' }) {
     return (
         <div>
             <img src={logo} alt='CRADLE' style={{ width: width, height: height }} />
