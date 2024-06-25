ec8e92a503e4a8d98f496bbdff75b38ac9ac437a
Babu-on-Github <d.i.popovici-1@student.tudelft.nl>
2024-06-25 13:47:23 +0200

    Changes

--- src/renderer/src/utils/entityDefinitions/entityDefinitions.js
+++ src/renderer/src/utils/entityDefinitions/entityDefinitions.js
@@ -37,3 +37,41 @@ export const entityTypes = new Set(['actor', 'case', 'entry', 'metadata']);
  * @type {Set<string>}
  */
 export const entityTypesReduced = new Set(['actor', 'case', 'entry']);
+
+/**
+ * Type for objects with style options for each entity type
+ *
+ * @Typedef {Object} EntityStyleOptions
+ * @property {string} actor - The style for actors
+ * @property {string} case - The style for cases
+ * @property {string} entry - The style for entries
+ * @property {string} metadata - The style for metadata
+ */
+
+/**
+ * entityGraphColors - This object contains the colors for the different entity types in the graph.
+ * The colors are used to distinguish between different types of entities in the graph.
+ * The keys are the entity types, and the values are the corresponding hex color codes.
+ *
+ * @type {EntityStyleOptions}
+ */
+export const entityGraphColors = {
+    actor: '#1a85ff',
+    case: '#744abf',
+    entry: '#e66100',
+    metadata: '#d41159',
+};
+
+/**
+ * entityMarkdownColors - This object contains the colors for the different entity types in the markdown.
+ * The colors are used to distinguish between different types of entities in the markdown.
+ * The keys are the entity types, and the values are the corresponding tailwindcss classes.
+ *
+ * @type {EntityStyleOptions}
+ */
+export const entityMarkdownColors = {
+    actors: 'text-[#1a85ff]',
+    cases: 'text-[#744abf]',
+    entries: 'text-[#e66100]',
+    metadata: 'text-[#d41159] underline',
+};
