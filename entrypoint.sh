a4d7596c23cefa8ad6b37c1434c978a3e2d9cefc
arg3t <yigit.colakoglu@prodaft.com>
2024-09-03 23:06:30 +0200

    Entrypoint improved

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,6 @@
 
 pipenv run python manage.py makemigrations
 pipenv run python manage.py migrate
+pipenv run python manage.py loaddata entries
 pipenv run python manage.py initadmin
 pipenv run gunicorn -b 0.0.0.0:8000 cradle.wsgi:application
