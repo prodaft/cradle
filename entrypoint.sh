7cf480bc5715735b4db402e4fd87a5b211bcae3c
arg3t <yigit.colakoglu@prodaft.com>
2024-09-05 14:05:43 +0200

    Add delete hanging entries to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,5 @@ pipenv run python manage.py makemigrations
 pipenv run python manage.py migrate
 pipenv run python manage.py loaddata entries
 pipenv run python manage.py initadmin
+pipenv run python manage.py delete_hanging_entries
 pipenv run gunicorn -b 0.0.0.0:8000 cradle.wsgi:application
