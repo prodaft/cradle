25484a5a7661d411833b804f9c5598b92aaf69bd
arg3t <yigit.colakoglu@prodaft.com>
2025-02-24 16:35:11 +0100

    Ignore fixtures variable

--- docker/entrypoint_backend.sh
+++ docker/entrypoint_backend.sh
@@ -4,7 +4,11 @@ pipenv run python manage.py collectstatic --noinput -c
 chown -R www-data:www-data static
 
 pipenv run python manage.py migrate
-pipenv run python manage.py loaddata entries
+
+if $INITIALIZE_ENTRYCLASSES; then
+  pipenv run python manage.py loaddata entries
+fi
+
 pipenv run python manage.py initadmin
 pipenv run python manage.py delete_hanging_entries
 
