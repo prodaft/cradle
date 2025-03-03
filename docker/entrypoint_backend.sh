d548ec694bc79e1358649e7647ccfffa23089b8d
Emre <emre@prodaft.com>
2025-03-03 09:06:43 +0100

    Increased worker count in web app

--- docker/entrypoint_backend.sh
+++ docker/entrypoint_backend.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+NUM_WORKERS=12
+
 pipenv run python manage.py collectstatic --noinput -c
 chown -R www-data:www-data static
 
@@ -12,4 +14,4 @@ fi
 pipenv run python manage.py initadmin
 pipenv run python manage.py delete_hanging_entries
 
-pipenv run gunicorn -b 0.0.0.0:8000 cradle.wsgi:application
+pipenv run gunicorn --workers $NUM_WORKERS -b 0.0.0.0:8000 cradle.wsgi:application
