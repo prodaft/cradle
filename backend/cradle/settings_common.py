0aeb7d3ed2dca8cecdd179d54d46df1dd7ef69fc
arg3t <yigit.colakoglu@prodaft.com>
2025-06-09 15:27:50 +0200

    Bump version to 2.3.0.
    - Add a file search field to look for all files in the system

--- backend/cradle/settings_common.py
+++ backend/cradle/settings_common.py
@@ -6,7 +6,7 @@ import os
 from datetime import timedelta
 from pathlib import Path
 
-VERSION = "2.2.4"
+VERSION = "2.3.0"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
