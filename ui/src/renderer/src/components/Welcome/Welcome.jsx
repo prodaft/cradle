e5ff3ccf51f52a97047602032c71fda205d2a4e5
Yigit Colakoglu <30830979+arg3t@users.noreply.github.com>
2025-07-21 14:34:32 +0000

    Bump version: 2.7.1 → 2.8.0

--- ui/src/renderer/src/components/Welcome/Welcome.jsx
+++ ui/src/renderer/src/components/Welcome/Welcome.jsx
@@ -76,7 +76,7 @@ export default function Welcome() {
                             className='mt-6 !text-sm !font-normal text-zinc-500 hover:cursor-pointer hover:opacity-80'
                             onClick={() => window.open('https://prodaft.com')}
                         >
-                            Copyright &copy; 2025 PRODAFT | v2.7.1
+                            Copyright &copy; 2025 PRODAFT | v2.8.0
                         </p>
                     </div>
                 </footer>
