03927cb1cfd7a426a00cc50b901567162f2656a8
arg3t <yigit.colakoglu@prodaft.com>
2025-12-04 12:55:25 +0100

    Fixed electron

--- ui/electron.vite.config.ts
+++ ui/electron.vite.config.ts
@@ -1,10 +1,22 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'electron-vite';
+import path from 'path';
 
 export default defineConfig({
     main: {},
     preload: {},
     renderer: {
         plugins: [react()],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src/renderer/src'),
+                '@components': path.resolve(__dirname, './src/renderer/src/components'),
+                '@contexts': path.resolve(__dirname, './src/renderer/src/contexts'),
+                '@hooks': path.resolve(__dirname, './src/renderer/src/hooks'),
+                '@services': path.resolve(__dirname, './src/renderer/src/services'),
+                '@utils': path.resolve(__dirname, './src/renderer/src/utils'),
+                '@types': path.resolve(__dirname, './src/renderer/src/types'),
+            },
+        },
     },
 });
