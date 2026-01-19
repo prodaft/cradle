85cb428bde8b789a3beac15b4786bc7681bde124
3xp0rt <61662492+3xp0rt@users.noreply.github.com>
2026-01-19 11:06:07 +0200

    refactor(ui): reorganize component exports and update structure

---
+++ ui/src/components/domain/graph/index.ts
@@ -0,0 +1,12 @@
+export { default as ExplorerPanel } from './ExplorerPanel';
+export { default as Graph } from './Graph';
+export { default as GraphControl } from './GraphControl';
+export { default as GraphExplorer } from './GraphExplorer';
+export { default as GraphFilters } from './GraphFilters';
+export { default as GraphLegend } from './GraphLegend';
+export { default as GraphQuery } from './GraphQuery';
+export { default as GraphSettings } from './GraphSettings';
+export { default as KnowledgeGraphSearch } from './KnowledgeGraphSearch';
+export { default as NoteGraphSearch } from './NoteGraphSearch';
+export type { Node, Edge } from './graphFilterUtils';
+export { filterNodes, filterEdges, filterGraph } from './graphFilterUtils';
